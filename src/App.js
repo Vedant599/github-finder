dbad59f7badf4854a27b2111d26dbcce59c614ae
Vedant599 <dynamicvedant@gmail.com>
2020-08-13 14:45:03 +0530

    Context Api Complete with Page Not Found

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
+import Home from './components/pages/Home';
+import PageNotFound from './components/pages/PageNotFound';
 
 const App = () => {
   return (
@@ -21,22 +21,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route path='/about' component={About} />
-                <Route
-                  exact
-                  path='/user/:user_login'
-                  component={User}
-                />
+                <Route exact path='/user/:user_login' component={User} />
+                <Route component={PageNotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div className='card text-center'>
+      <h1>Error: 404</h1>
+      <h2>Sorry The Page You Requested was not Found</h2>
+    </div>
+  );
+};
+export default PageNotFound;
